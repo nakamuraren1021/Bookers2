057482dc8cdaf73c2d26485d17558df68e70e3a1
Your Name <you@example.com>
2019-10-26 11:00:19 +0000

    Finish[5-9]

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '74dd8cc6758e6f740b3d7f9088c241227efe6386e2b17b1d411d42b8c8315db9ffe984753f9da2b741b75e61d43cf8777ba11aa9addcc42a47a2e28375895752'
---
+++ db/migrate/20191019065921_add_user_id.rb
@@ -0,0 +1,4 @@
+class AddUserId < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
